f8e9b36bb2bd125b301051ef44f7e1f643f0b03e
Shachar David <shachadavid2013@gmail.com>
2023-05-26 20:03:21 +0300

    all functions completed, need to run

--- models.py
+++ models.py
@@ -68,11 +68,16 @@ class Board:
         self.canvas = None
         self.board_size = board_size
         self.points_distance = points_distance
-        self.h_lines = [[None] * board_size] * board_size
-        self.v_lines = [[None] * board_size] * board_size
+        self.h_lines: List[List[Line]] = [[None] * (board_size - 1)] * board_size
+        self.v_lines: List[List[Line]] = [[None] * board_size] * (board_size - 1)
+        self.boxes: List[List[Box]] = [[None] * (board_size - 1)] * (board_size - 1)
         self.points: List[Point] = []
         self.draw_points()
         self.current_player = 0
+        self.last_line: Line = None
+
+    def switch_players(self):
+        self.current_player = abs(self.current_player - 1)
 
     def init_canvas(self):
         root = tk.Tk()
@@ -82,8 +87,9 @@ class Board:
         self.canvas.pack()
 
     def draw_points(self):
-        self.points = [[Point(x * self.points_distance, y * self.points_distance) for x in range(self.board_size)] for y
-                       in range(self.board_size)]
+        self.points = [
+            [Point(x * self.points_distance, y * self.points_distance, self.canvas) for x in range(self.board_size)]
+            for y in range(self.board_size)]
         for row in self.points:
             for point in row:
                 point.draw()
@@ -92,12 +98,23 @@ class Board:
         return 'red' if self.current_player else 'blue'
 
     def set_line(self, x1, y1, x2, y2):
+        line = Line(self.points[x1][y1], self.points[x2][y2], self.get_color(), self.canvas)
         if x1 == x2:
-            self.h_lines.append(Line(self.points[x1][y1], self.points[x2][y2], self.get_color(), self.canvas))
+            if not self.h_lines[x1][y1]:
+                self.h_lines[x1][y1] = line
         if y1 == y2:
-            self.v_lines.append(Line(self.points[x1][y1], self.points[x2][y2], self.get_color(), self.canvas))
+            if not self.v_lines[x1][y1]:
+                self.v_lines[x1][y1] = line
+        else:
+            return
+        self.last_line = line
+        self.check_completed_boxes()
+        self.switch_players()
 
     def check_completed_boxes(self):
         for i in range(self.board_size - 1):
             for j in range(self.board_size - 1):
                 if self.h_lines[i][j] and self.h_lines[i + 1][j] and self.v_lines[i][j] and self.v_lines[i][j + 1]:
+                    self.boxes[i][j] = Box(self.h_lines[i][j].p1, self.h_lines[i + 1][j].p2, self.last_line,
+                                           self.canvas)
+                    self.boxes[i][j].draw()
